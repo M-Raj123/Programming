34829e7ea87650bcf69e824eac51fe6a9adaf21e
RAJAPAUL <61533823+M-Raj123@users.noreply.github.com>
2023-02-15 12:12:41 +0530

    declare strrev()

--- C/String Functions/mylib.h
+++ C/String Functions/mylib.h
@@ -3,3 +3,4 @@ char* stringCopy(char *dest, char *src);
 char* stringNCopy(char* dest, char* src, int n);
 char* stringConcat(char *str1, char *str2);
 char* stringChar(char *str, int a);
+char* stringReverse(char *s);
