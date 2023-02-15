2a8cc9bfb780e90998f387e8925ff09e5ddb5854
RAJAPAUL <61533823+M-Raj123@users.noreply.github.com>
2023-02-15 12:19:02 +0530

    declare memmove()

--- C/String Functions/mylib.h
+++ C/String Functions/mylib.h
@@ -4,4 +4,5 @@ char* stringNCopy(char* dest, char* src, int n);
 char* stringConcat(char *str1, char *str2);
 char* stringChar(char *str, int a);
 char* stringReverse(char *s);
-char* memCpy(char *dest, char *src,int size);
+char* memCpy(char *dest, char *src, int size);
+char* memMove(char *dest, char *src, int size );
