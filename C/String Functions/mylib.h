0a3eb0147f669efc6ae397c756bac6986f3e5da0
RAJAPAUL <61533823+M-Raj123@users.noreply.github.com>
2023-02-15 12:03:53 +0530

    declare strchr()

--- C/String Functions/mylib.h
+++ C/String Functions/mylib.h
@@ -1,4 +1,5 @@
 int findStrLen(char *str); 
 char* stringCopy(char *dest, char *src);
 char* stringNCopy(char* dest, char* src, int n);
-char* stringConcat(char *str1,char *str2);
+char* stringConcat(char *str1, char *str2);
+char* stringChar(char *str, int a);
